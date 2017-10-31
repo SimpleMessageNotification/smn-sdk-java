a7c0bdd77d8bef2e2f7fffe78dd54e508201e89b
l00307761 <smndeveloper@huawei.com>
2017-10-31 16:07:13 +0800

    填加版权

--- smn-sdk-java/src/main/java/com/smn/exception/ClientException.java
+++ smn-sdk-java/src/main/java/com/smn/exception/ClientException.java
@@ -1,3 +1,20 @@
+/*
+ * ====================================================================
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements. See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership. The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
 package com.smn.exception;
 
 import com.smn.common.ErrorCode;
@@ -9,6 +26,9 @@ import com.smn.common.ErrorCode;
  * 用于表示客户端无法处理的消息, 该请求无法被继续处理
  * 比如: 参数为空
  * </p>
+ *
+ * @author zhangyx
+ * @version 0.9
  */
 public class ClientException extends RuntimeException {
 
