22cddd83bce780adb0da1de6608a94ee31131fe1
l00307761 <smndeveloper@huawei.com>
2017-11-02 09:01:42 +0800

    修复参数限制bug

--- smn-sdk-java/src/main/java/com/smn/common/SmnConfiguration.java
+++ smn-sdk-java/src/main/java/com/smn/common/SmnConfiguration.java
@@ -17,14 +17,14 @@
  */
 package com.smn.common;
 
+import org.apache.commons.lang3.StringUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.util.Properties;
-
-import org.apache.commons.lang3.StringUtils;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 /**
  * property loading configuration
