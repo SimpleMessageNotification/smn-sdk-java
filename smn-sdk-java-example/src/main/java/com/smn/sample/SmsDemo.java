c0c1240e7ecbd7e91726611e4a784dede096afea
l00307761 <smndeveloper@huawei.com>
2017-11-02 13:10:37 +0800

    sign list. sign delete

--- smn-sdk-java-example/src/main/java/com/smn/sample/SmsDemo.java
+++ smn-sdk-java-example/src/main/java/com/smn/sample/SmsDemo.java
@@ -4,19 +4,25 @@ public class SmsDemo {
 
     public static void main(String[] args) {
         ClientDemo clientDemo = new ClientDemo();
-        // 发送消息
-        clientDemo.smsPublish();
+//        // 发送消息
+//        clientDemo.smsPublish();
+//
+//        //查询短信的发送状态的demo
+//        clientDemo.listSmsMsgReport();
+//
+//        // 查询短信的内容
+//        clientDemo.getSmsMessage();
+//
+//        // 查询短信回调事件的demo
+//        clientDemo.listSmsCallbackEvent();
+//
+//        //更新短信回调事件
+//        clientDemo.updateSmsCallbackEvent();
 
-        //查询短信的发送状态的demo
-        clientDemo.listSmsMsgReport();
+        //查询短信签名
+//        clientDemo.listSmsSigns();
 
-        // 查询短信的内容
-        clientDemo.getSmsMessage();
-
-        // 查询短信回调事件的demo
-        clientDemo.listSmsCallbackEvent();
-
-        //更新短信回调事件
-        clientDemo.updateSmsCallbackEvent();
+        //删除签名
+        clientDemo.deleteSmsSign();
     }
 }
