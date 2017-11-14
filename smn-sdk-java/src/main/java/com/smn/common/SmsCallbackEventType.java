0a99f41dd0bcac175d143942f220c1c68a92de45
l00307761 <smndeveloper@huawei.com>
2017-11-14 17:54:24 +0800

    添加LICENSE/版权和免责声明，修改版本号1.0.0

--- smn-sdk-java/src/main/java/com/smn/common/SmsCallbackEventType.java
+++ smn-sdk-java/src/main/java/com/smn/common/SmsCallbackEventType.java
@@ -1,5 +1,22 @@
+/*
+ * Copyright (C) 2017. Huawei Technologies Co., LTD. All rights reserved.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of Apache License, Version 2.0.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * Apache License, Version 2.0 for more details.
+ */
 package com.smn.common;
 
+/**
+ * sms callback event type
+ *
+ * @author zhangyx
+ * @version 0.9
+ */
 public final class SmsCallbackEventType {
 
     private SmsCallbackEventType() {
